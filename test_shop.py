f7f3d5dd171e3a7f6009bfee6937bb8ff2acb90d
GoodNight0911 <morozovergei.frost@gmail.com>
2026-08-20 23:10:51 +0300

    feat: update assert to check cart url

--- test_shop.py
+++ test_shop.py
@@ -5,25 +5,20 @@ def test_add_phone_to_cart(page: Page):
     # 1. Открываем интернет-магазин гаджетов
     page.goto("https://demoblaze.com")
 
-    # 2. Находим по тексту ссылку на Samsung Galaxy S6 и кликаем по ней
+    # 2. Переходим в карточку Samsung
     page.get_by_role("link", name="Samsung galaxy s6").click()
 
-    # 3. Находим кнопку "Add to cart" по её тексту и нажимаем
-    # Используем точное совпадение текста
-    page.get_by_text("Add to cart").click()
-
-    # 4. Сайт выдаст браузерное alert-окно,
-    # мы просим Playwright дождаться появления диалога и автоматически нажать "ОК"
+    # 3. СНАЧАЛА включаем перехватчик всплывающего окна (dialog)
     page.on("dialog", lambda dialog: dialog.accept())
 
-    # Пауза 4 секунды, чтобы увидеть, как нажалась кнопка добавления(в рабочих задачах не использовать)
-    # page.wait_for_timeout(4000)
+    # 4. Теперь кликаем "Add to cart" — окно выскочит и тут же закроется само
+    page.get_by_text("Add to cart").click()
 
-    # 5. Кликаем по кнопке "Cart" (Корзина) в верхнем навигационном меню
+    # 5. Кликаем по кнопке "Cart" (Корзина) в верхнем меню
     page.get_by_role("link", name="Cart", exact=True).click()
 
-    # 6. Ждем, пока в таблице корзины появится строка с нашим телефоном
-    page.locator("td:has-text('Samsung galaxy s6')").wait_for(state="visible")
+    # 6. Ждем правильный URL
+    page.wait_for_url("**/cart.html")
 
-    # 7. ГЛАВНАЯ ПРОВЕРКА: убеждаемся, что этот текст физически виден на экране
-    assert page.locator("td:has-text('Samsung galaxy s6')").is_visible()
+    # 7. Проверяем, что адрес сайта теперь точно равен странице корзины
+    assert page.url == "https://demoblaze.com/cart.html"
